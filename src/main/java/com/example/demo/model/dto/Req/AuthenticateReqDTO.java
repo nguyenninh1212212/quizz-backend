25e73bcff34654608a71a56eda03b8e1f7585f98
nguyenanninh1212212 <ninh56085@gmail.com>
2025-05-19 14:47:16 +0700

    add

--- src/main/java/com/example/demo/model/dto/Req/AuthenticateReqDTO.java
+++ src/main/java/com/example/demo/model/dto/Req/AuthenticateReqDTO.java
@@ -1,5 +1,9 @@
 package com.example.demo.model.dto.Req;
 
+import com.example.demo.validator.LoginValidateGroup;
+import com.example.demo.validator.RegisterValidateGroup;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +15,23 @@ import lombok.NonNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticateReqDTO {
-    @NonNull
-    private String username;
 
     @NonNull
+    @NotBlank(message = "Age cannot be blank", groups = { LoginValidateGroup.class, RegisterValidateGroup.class })
+    private Integer age;
+
+    @NotBlank(message = "Username cannot be blank", groups = { LoginValidateGroup.class, RegisterValidateGroup.class })
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank", groups = { LoginValidateGroup.class, RegisterValidateGroup.class })
     private String password;
+
+    @NotBlank(message = "Fullname cannot be blank", groups = RegisterValidateGroup.class)
+    private String fullname;
+
+    @NotBlank(message = "Email cannot be blank", groups = RegisterValidateGroup.class)
+    private String email;
+
+    @NotBlank(message = "Phone number cannot be blank", groups = RegisterValidateGroup.class)
+    private String phoneNumber;
 }
