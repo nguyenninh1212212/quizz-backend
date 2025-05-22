4366c9868fbd35e84a8d658e783b20bf1d7bb43b
nguyenanninh1212212 <ninh56085@gmail.com>
2025-05-22 13:59:50 +0700

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.demo.repository")
 @EntityScan(basePackages = "com.example.demo.model")
 public class DemoApplication {
 	public static void main(String[] args) {
+		Dotenv.configure().load();
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
